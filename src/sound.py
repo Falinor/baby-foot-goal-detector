9cf1b16715d76ad986e5f96b581c06fbaaa754b7
Andrea Gueugnaut <agueugnaut@octo.com>
2020-12-12 15:13:24 +0100

    refactor(sound): replace pydub by pygame

--- src/sound.py
+++ src/sound.py
@@ -1,25 +1,24 @@
 import glob
+from pygame.mixer import init, music
+import time
+
 from os.path import join
 from itertools import cycle
-from pydub import AudioSegment
-from pydub.playback import play
 
 
 class Sound:
     def __init__(self, path):
         self.path = path
-        self.sound = AudioSegment.from_mp3(path)
+        self.sound = music.load(path)
 
     def play(self):
-        play(self.sound)
+        music.play()
 
 class Playlist:
     def __init__(self, directory):
-        self.songs = [
-          AudioSegment.from_mp3(mp3_file)
-          for mp3_file
-          in glob(join(directory, "*.mp3"))
-        ]
+        self.songs = glob(join(directory, "*.mp3"))
 
     def next(self):
         return cycle(self.songs)
+
+init()
